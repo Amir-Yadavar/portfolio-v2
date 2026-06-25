e3f1d9d74d444aa7f59121bb9ba648a8932118f1
Amir-Yadavar <amiresya@gmail.com>
2026-06-25 11:33:25 +0330

    create tours in sphere ..

--- components/module/HeroCanvas/HeroCanvas.tsx
+++ components/module/HeroCanvas/HeroCanvas.tsx
@@ -4,6 +4,7 @@ import { OrbitControls, useTexture } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { Bloom, EffectComposer } from "@react-three/postprocessing";
+import { useControls } from "leva";
 
 function AnimatedSphere() {
   const sphereRef = useRef<THREE.Mesh | null>(null);
@@ -19,7 +20,7 @@ function AnimatedSphere() {
 
   return (
     <mesh ref={sphereRef}>
-      <sphereGeometry args={[2.5, 64, 64]} />
+      <sphereGeometry args={[2, 64, 64]} />
       <meshPhysicalMaterial
         color="#000000"
         emissive="#a855f7"
@@ -38,20 +39,90 @@ function AnimatedSphere() {
   );
 }
 
+function Torus() {
+  const ring1Ref = useRef<THREE.Mesh | null>(null);
+  const ring2Ref = useRef<THREE.Mesh | null>(null);
+
+  // محاسبه موقعیت ۵ ستاره دور دایره
+  const totalStars = 5;
+  const starPositions = Array.from({ length: totalStars }).map((_, index) => {
+    const angle = (index / totalStars) * Math.PI * 2;
+    return [2.5 * Math.cos(angle), 2.5 * Math.sin(angle), 0];
+  });
+
+  const localAxis = new THREE.Vector3(0, 0, 1);
+
+  useFrame(() => {
+    if (ring1Ref.current) {
+      ring1Ref.current.rotateOnAxis(localAxis, 0.002);
+    }
+    if (ring2Ref.current) {
+      ring2Ref.current.rotateOnAxis(localAxis, 0.002); 
+    }
+  });
+
+  return (
+    <group>
+      {/* میله اول با مقادیر عددی ثابت شما */}
+      <mesh ref={ring1Ref} rotation={[0, -0.02, 0]}>
+        <torusGeometry args={[2.5, 0.01, 16, 100]} />
+        <meshStandardMaterial
+          color="#3e2852"
+          emissive="#3e2852"
+          emissiveIntensity={0.1}
+        />
+
+        {starPositions.map((pos, i) => (
+          <mesh key={i} position={pos as [number, number, number]}>
+            <sphereGeometry args={[0.04, 4, 4]} />
+            <meshStandardMaterial
+              color="#c084fc"
+              emissive="#a855f7"
+              emissiveIntensity={4}
+              toneMapped={false}
+            />
+          </mesh>
+        ))}
+      </mesh>
+      
+      {/* میله دوم با مقادیر عددی ثابت شما */}
+      <mesh ref={ring2Ref} rotation={[-1.4, 0.32, 0]}>
+        <torusGeometry args={[2.5, 0.01, 16, 100]} />
+        <meshStandardMaterial
+          color="#3e2852"
+          emissive="#3e2852"
+          emissiveIntensity={0.1}
+        />
+        {starPositions.map((pos, i) => (
+          <mesh key={i} position={pos as [number, number, number]}>
+            <sphereGeometry args={[0.04, 4, 4]} />
+            <meshStandardMaterial
+              color="#c084fc"
+              emissive="#a855f7"
+              emissiveIntensity={4}
+              toneMapped={false}
+            />
+          </mesh>
+        ))}
+      </mesh>
+    </group>
+  );
+}
+
 export default function HeroCanvas() {
   return (
     <Canvas gl={{ alpha: true, antialias: true }}>
       <ambientLight intensity={1} />
       {/* <directionalLight position={[3, 2, 4]} intensity={1} /> */}
       <OrbitControls />
-      <pointLight position={[0, 0, -5]} intensity={5} color="#a855f7" />
-      {/* اینجا داخل Canvas هست */}
+      {/* <pointLight position={[0, 0, -5]} intensity={2} color="#a855f7" /> */}
+      
       <AnimatedSphere />
-
+      <Torus />
       <EffectComposer>
         <Bloom
-          intensity={2}
-          luminanceThreshold={0.1}
+          intensity={1.5}
+          luminanceThreshold={0.5}
           luminanceSmoothing={0.9}
         />
       </EffectComposer>
